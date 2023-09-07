5573612e908691d3b4f8cf4bb5edbcfd9330d1cb
bogda <bogdan.nelepa01@mail.ru>
2023-09-07 22:34:55 +0500

    KS-2-2 average salary

---
+++ src/main/kotlin/lesson2/lesson2-task2.kt
@@ -0,0 +1,14 @@
+package lesson2
+fun main () {
+
+    var companyEmployees = 50
+    var salary: Int = 30000
+    var interns = 30
+    var salaryInterns = 20000
+    var staff = companyEmployees + interns
+    var salaryCompanyEmployees = companyEmployees * salary
+    var expenses = (interns * salaryInterns) + salaryCompanyEmployees
+    var averageSalary = expenses / staff
+             println(averageSalary)
+
+}
