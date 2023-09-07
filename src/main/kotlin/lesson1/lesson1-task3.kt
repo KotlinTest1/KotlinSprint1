3690075285999275adbd9518891f033095fc44af
bogda <bogdan.nelepa01@mail.ru>
2023-09-07 16:36:36 +0500

    KS-1-3 number of employees

--- src/main/kotlin/lesson1/lesson1-task3.kt
+++ src/main/kotlin/lesson1/lesson1-task3.kt
@@ -2,15 +2,10 @@ package les
 fun main() {
 
     val year = 1961
-    var hour = 9
-    var minute = 7
-
-    //println(year)
-    //println(hour)
-    //println(minute)
-
-    hour = 10
-    minute = 55
+    var hour = "09"
+    var minute = "07"
+    hour = "10"
+    minute = "55"
     var separation = ":"
 
     print(hour)
