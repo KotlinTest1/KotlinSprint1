ba01c44e8e58bfa1aa998dab829dd36e265c0640
bogda <bogdan.nelepa01@mail.ru>
2023-09-11 19:47:29 +0500

    KS-3-3 multiplication table

--- src/main/kotlin/lesson3/lesson3-task3.kt
+++ src/main/kotlin/lesson3/lesson3-task3.kt
@@ -1,7 +1,20 @@
 package lesson3
 fun main () {
 
-    var number = 5
+    var number = """
+        
+        5 х 1 = 5
+        5 х 2 = 10
+        5 х 3 = 15
+        5 х 4 = 20
+        5 х 5 = 25
+        5 х 6 = 30
+        5 х 7 = 35
+        5 х 8 = 40
+        5 х 9 = 45
+        5 х 10 = 50
+        
+    """.trimIndent()
+    println(number)
 
-    println("${number * 1} ${number * 2} ${number * 3} ${number * 4} ${number * 5} ${number * 6} ${number * 7} ${number * 8} ${number * 9}")
 }
