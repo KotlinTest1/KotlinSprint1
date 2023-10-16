ef4ca5f52cfcc325f03baa63c5f3defd4e271d12
bogda <bogdan.nelepa01@mail.ru>
2023-10-16 13:05:29 +0500

    KS-4-1 Tables booked

---
+++ src/main/kotlin/Lesson4/Lesson4-task1.kt
@@ -0,0 +1,10 @@
+ fun main() {
+
+     var bookedToday = 13
+     var bookedTomorrow = 9
+     var comparisonResult: Boolean = bookedToday < NUMBER_OF_TABLES
+     var comparisonResult2: Boolean = bookedTomorrow < NUMBER_OF_TABLES
+        println("Доступность столиков на сегодня: $comparisonResult")
+        println("Доступность столиков на сегодня: $comparisonResult2")
+}
+ const val NUMBER_OF_TABLES = 13
