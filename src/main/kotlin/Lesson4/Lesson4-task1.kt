0ccfa1634f911870a5472d33272f1a59559624b2
bogda <bogdan.nelepa01@mail.ru>
2023-10-17 13:04:53 +0500

    KS-4-2 Averege weigth and volume

--- src/main/kotlin/Lesson4/Lesson4-task1.kt
+++ src/main/kotlin/Lesson4/Lesson4-task1.kt
@@ -1,10 +1,8 @@
  fun main() {
 
-     var bookedToday = 13
+     val bookedToday = 13
      var bookedTomorrow = 9
-     var comparisonResult: Boolean = (bookedToday < NUMBER_OF_TABLES) && (bookedTomorrow < NUMBER_OF_TABLES)
-     //var comparisonResult2: Boolean =
-        println("Доступность столиков на сегодня: $comparisonResult, Доступность столиков на завтра: $comparisonResult")
+        println("Доступность столиков на сегодня: ${bookedToday < NUMBER_OF_TABLES} Доступность столиков на завтра: ${bookedTomorrow < NUMBER_OF_TABLES} ")
+ }
 
-}
  const val NUMBER_OF_TABLES = 13
