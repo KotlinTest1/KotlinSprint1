5bca4814d03ecc1446600ff6a638223fc7594432
bogda <bogdan.nelepa01@mail.ru>
2023-10-17 13:18:07 +0500

    KS-4-2 Averege weigth and volume

--- src/main/kotlin/Lesson4/Lesson4-task1.kt
+++ src/main/kotlin/Lesson4/Lesson4-task1.kt
@@ -1,7 +1,7 @@
  fun main() {
 
      val bookedToday = 13
-     var bookedTomorrow = 9
+     val bookedTomorrow = 9
         println("Доступность столиков на сегодня: ${bookedToday < NUMBER_OF_TABLES} Доступность столиков на завтра: ${bookedTomorrow < NUMBER_OF_TABLES} ")
  }
 
