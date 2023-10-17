3f82a056adc54a53391996831784c20a20d39e45
bogda <bogdan.nelepa01@mail.ru>
2023-10-17 13:24:57 +0500

    KS-4-2 Averege weigth and volume

--- src/main/kotlin/Lesson4/Lesson4-task2.kt
+++ src/main/kotlin/Lesson4/Lesson4-task2.kt
@@ -11,24 +11,3 @@
  const val AVEREGE_WEIGHT_MIN = 35
  const val AVEREGE_WEIGHT_MAX = 100
  const val VOLUME_MAX = 100
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //var comparisonResult: Boolean = (weight1 >= AVEREGE_WEIGHT_MIN) && (volume1 < VOLUME_MAX)
- //var comparisonResult2: Boolean = (weight2 >= AVEREGE_WEIGHT_MIN) && (volume2 < VOLUME_MAX)
- //var comparisonResult: Boolean = (weight1 in AVEREGE_WEIGHT_MIN .. AVEREGE_WEIGHT_MAX) && (volume1 in VOLUME_MAX .. VOLUME_MAX)
- //var comparisonResult2: Boolean = (weight2 in AVEREGE_WEIGHT_MIN.. AVEREGE_WEIGHT_MAX) && (volume2 in VOLUME_MAX.. VOLUME_MAX)
