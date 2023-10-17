c79f01b4ac7b8dc8f4a515948219950c873d8106
bogda <bogdan.nelepa01@mail.ru>
2023-10-17 13:24:20 +0500

    KS-4-2 Averege weigth and volume

--- src/main/kotlin/Lesson4/Lesson4-task2.kt
+++ src/main/kotlin/Lesson4/Lesson4-task2.kt
@@ -3,14 +3,32 @@
      val volume1 = 80
      val weight2 = 50
      val volume2 = 100
-     //var comparisonResult: Boolean = (weight1 >= AVEREGE_WEIGHT_MIN) && (volume1 < VOLUME_MAX)
-     //var comparisonResult2: Boolean = (weight2 >= AVEREGE_WEIGHT_MIN) && (volume2 < VOLUME_MAX)
-     var comparisonResult: Boolean = (weight1 in AVEREGE_WEIGHT_MIN .. AVEREGE_WEIGHT_MAX) && (volume1 in VOLUME_MAX .. VOLUME_MAX)
-     var comparisonResult2: Boolean = (weight2 in AVEREGE_WEIGHT_MIN.. AVEREGE_WEIGHT_MAX) && (volume2 in VOLUME_MAX.. VOLUME_MAX)
 
-        println("Груз с весом $weight1 и объемом $volume1: $comparisonResult")
-        println("Груз с весом $weight2 и объемом $volume2: $comparisonResult2")
+        println("Груз с весом ${weight1 >= AVEREGE_WEIGHT_MIN} и объемом ${volume1 <= VOLUME_MAX}")
+        println("Груз с весом ${weight2 >= AVEREGE_WEIGHT_MIN} и объемом ${volume2 <= VOLUME_MAX}")
 }
+
  const val AVEREGE_WEIGHT_MIN = 35
  const val AVEREGE_WEIGHT_MAX = 100
  const val VOLUME_MAX = 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //var comparisonResult: Boolean = (weight1 >= AVEREGE_WEIGHT_MIN) && (volume1 < VOLUME_MAX)
+ //var comparisonResult2: Boolean = (weight2 >= AVEREGE_WEIGHT_MIN) && (volume2 < VOLUME_MAX)
+ //var comparisonResult: Boolean = (weight1 in AVEREGE_WEIGHT_MIN .. AVEREGE_WEIGHT_MAX) && (volume1 in VOLUME_MAX .. VOLUME_MAX)
+ //var comparisonResult2: Boolean = (weight2 in AVEREGE_WEIGHT_MIN.. AVEREGE_WEIGHT_MAX) && (volume2 in VOLUME_MAX.. VOLUME_MAX)
