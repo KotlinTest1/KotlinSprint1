f60591423f0189c99b77a24c38848eb417553ff5
bogda <bogdan.nelepa01@mail.ru>
2023-10-18 13:05:43 +0500

    KS-4-3 automatic monitoring system for plant growth conditions.

--- src/main/kotlin/Lesson4/Lesson4-task3.kt
+++ src/main/kotlin/Lesson4/Lesson4-task3.kt
@@ -1,13 +1,13 @@
 fun main () {
 
-    val todaysWeather: Boolean = true
-    val tentToday: Boolean = true
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
     val airHumidity: Int = 20
     val currentSeason: String = "Зима"
-    val comparisonResult = (tentToday == TODAYS_WEATHER) && (tentToday == TENT_TODAY) && (airHumidity == AIR_HUMIDITY) && (currentSeason == CURRENT_SEASON)
+    val comparisonResult = (isSunny == IS_SUNNY) && (isAwningOpen == IS_AWNINGOPEN) && (airHumidity == AIR_HUMIDITY) && (currentSeason == CURRENT_SEASON)
         println("Благоприятные ли условия сейчас для роста бобовых? $comparisonResult")
 }
-const val TODAYS_WEATHER: Boolean = true
-const val TENT_TODAY: Boolean = true
+const val IS_SUNNY: Boolean = true
+const val IS_AWNINGOPEN: Boolean = true
 const val AIR_HUMIDITY: Int = 20
-const val CURRENT_SEASON: String = "Не зима"
+const val CURRENT_SEASON: String = "Осень"
