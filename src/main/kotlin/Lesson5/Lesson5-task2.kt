e551a75fb39c8de323339f61e4f32870862ea04a
bogda <bogdan.nelepa01@mail.ru>
2023-10-20 13:36:53 +0500

    KS-5-2 Authorization user year of birth

---
+++ src/main/kotlin/Lesson5/Lesson5-task2.kt
@@ -0,0 +1,13 @@
+fun main () {
+
+    val yearOfBirth = readln().toInt()
+
+    if (yearOfBirth == YAER_OF_BIRTH) println("Показать экран со скрытым контентом")
+
+    else if (yearOfBirth == 18) println("Показать экран со скрытым контентом")
+
+    else println("Главный экран")
+
+}
+const val AGE_OF_MAJORITY = 18
+const val YAER_OF_BIRTH = 2015
