d5bbc5216a7d4aba36746cd43ffc655a9b83c141
bogda <bogdan.nelepa01@mail.ru>
2023-10-30 19:05:43 +0500

    KS-5-1 Kapcha

--- src/main/kotlin/Lesson5/Lesson5-task1.kt
+++ src/main/kotlin/Lesson5/Lesson5-task1.kt
@@ -1,7 +1,6 @@
 fun main () {
 
     val numberOne = 2
-    val actionOnANumber = "+"
     val numberTwo = 2
     val responseToCaptcha = readln().toInt()
     if ((numberOne + numberTwo) == responseToCaptcha) {
