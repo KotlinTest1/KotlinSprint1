0d31d065ff252b531660a70e9c5f822aecbb8469
bogda <bogdan.nelepa01@mail.ru>
2023-11-07 18:56:24 +0500

    KS-5-1 Kapcha

--- src/main/kotlin/Lesson5/Lesson5-task1.kt
+++ src/main/kotlin/Lesson5/Lesson5-task1.kt
@@ -1,7 +1,8 @@
 fun main () {
-    println("Решите пример:")
+
     val numberOne = 2
     val numberTwo = 2
+    println("Решите пример: $numberOne + $numberTwo")
     val responseToCaptcha = readln().toInt()
     if ((numberOne + numberTwo) == responseToCaptcha) {
         println("Добро пожаловать!")
