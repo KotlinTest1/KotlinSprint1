ef07ec03d36de603ec45cdccfe589d8ac09f0bf2
bogda <bogdan.nelepa01@mail.ru>
2023-11-07 13:32:21 +0500

    KS-5-3 Lottery win

--- src/main/kotlin/Lesson5/lesson5-task3.kt
+++ src/main/kotlin/Lesson5/lesson5-task3.kt
@@ -7,9 +7,9 @@ fun main () {
         println("Введите второе число:")
     val secondNumber = readln().toInt()
         if (firstNumber == theFirstWinningNumber && secondNumber == theSecondWinningNumber) println("Поздравляем! Вы выиграли главный приз!")
-        else if (firstNumber == theSecondWinningNumber || secondNumber == firstNumber) println("Поздравляем! Вы выиграли главный приз!")
-
+        else if (firstNumber == theSecondWinningNumber || secondNumber == firstNumber ) println("Поздравляем! Вы выиграли главный приз!")
         else if (firstNumber == theFirstWinningNumber || secondNumber == theSecondWinningNumber) println("Вы выиграли утешительный приз!")
+        else if (firstNumber == theSecondWinningNumber || secondNumber == theFirstWinningNumber) println("Вы выиграли утешительный приз!")
         else println("Неудача!")
     println("Для победы необходимы числа: $theFirstWinningNumber,$theSecondWinningNumber")
 }
