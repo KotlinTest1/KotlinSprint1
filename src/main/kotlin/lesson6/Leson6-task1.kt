1439910b3a74f7c0b67019915b70c83f944c1d10
bogda <bogdan.nelepa01@mail.ru>
2023-11-11 14:44:50 +0500

    KS-6-1 User registration

--- src/main/kotlin/lesson6/Leson6-task1.kt
+++ src/main/kotlin/lesson6/Leson6-task1.kt
@@ -1,8 +1,11 @@
 fun main ()  {
 
+    println("Для регистрации введите логин и пароль:")
     val userName = readln()
     val userPassword = readln().toInt()
-
+    println("Для авторизации ввидете логин и пароль:")
+    readln().toString()
+    readln().toInt()
     do{
            println("Авторизация прошла успешно $userName $userPassword")
 
