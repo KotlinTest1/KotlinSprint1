e4e8da007471fe5bd715b14939a216e0f4accfe5
bogda <bogdan.nelepa01@mail.ru>
2023-11-11 13:16:46 +0500

    KS-6-1 User registration

---
+++ src/main/kotlin/lesson6/Leson6-task1.kt
@@ -0,0 +1,14 @@
+fun main ()  {
+
+    val userName = readln()
+    val userPassword = readln().toInt()
+
+    do{
+           println("Авторизация прошла успешно $userName $userPassword")
+
+    }while (true)
+
+
+
+
+}
